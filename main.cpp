3ed7d56869d711c9fad0f8342cd1957d2f482644
Sheng-Chien Lin <mlsaint1214@gmail.com>
2018-12-05 19:43:05 +0800

    add test case

--- main.cpp
+++ main.cpp
@@ -1,42 +1,47 @@
-#include<iostream>
-#include<vector>
-#include<unordered_map>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <fstream>
 
 class DicTreeNode
 {
 	public:
-		DicTreeNode(char val = '\0', unsigned int len = 0);
+		DicTreeNode(char val = '\0', off_t len = 0);
 		~DicTreeNode();
 		void addChildNode(char, class DicTreeNode *);
 		DicTreeNode* findLeaf(char *, off_t &);
 		void setEnd(bool);
 		bool getEnd();
-		off_t isMatchKeyword(char *, off_t);
+		class DicTreeNode *isMatchKeyword(char *, off_t, char * &);
 
-		static long search_count;
+		static long search_count; // compare counter
 
-	private:
 		bool isEnd; // to avoid "ABC" and "ABCD.." case
 		char value;
-		unsigned int length;
+		off_t length;
 		std::unordered_map < char, class DicTreeNode * > map; // use unorder map as tree
 		std::vector< class DicTreeNode * > childNodes; // for deconstructor
+		class DicTreeNode *parentNode;
+		class DicTreeNode *toFirstMatchNode; // KMP algo
 };
 
 long DicTreeNode::search_count = 0;
 
 
-DicTreeNode::DicTreeNode(char val, unsigned int len)
+DicTreeNode::DicTreeNode(char val, off_t len)
 {
 	value = val;
 	length = len;
+	toFirstMatchNode = NULL;
 }
 
 DicTreeNode::~DicTreeNode()
 {
+	map.clear();
 	for (auto const& child : childNodes) {
 		delete child;
 	}
+	childNodes.clear();
 }
 
 // To find the last common prefix node between keywords
@@ -76,13 +81,14 @@ bool DicTreeNode::getEnd()
 }
 
 //To find match keyword
-off_t DicTreeNode::isMatchKeyword(char *str, off_t maxLen)
+class DicTreeNode *DicTreeNode::isMatchKeyword(char *str, off_t maxLen, char * &out)
 {
 	DicTreeNode *result = this;
 	off_t ind = 0;
-	off_t retval = 0;
+	class DicTreeNode *retval = NULL;
 	std::unordered_map< char, class DicTreeNode *>::iterator it;
 	
+	out = str;
 	search_count++;
 	while ( ((it = result->map.find(str[ind])) !=  result->map.end()) && ind < maxLen) {
 		//std::cout << str[ind] << std::endl;
@@ -90,7 +96,14 @@ off_t DicTreeNode::isMatchKeyword(char *str, off_t maxLen)
 		ind++;
 		search_count++;
 		if (result->getEnd()) {
-			retval = ind;
+			retval = result;
+			out = str + ind;
+			//break;
+		} else {
+			if (retval == NULL && result->toFirstMatchNode != NULL) {
+				retval = result->toFirstMatchNode;
+				out = str + ind;
+			}
 		}
 	}
 	return retval;
@@ -99,25 +112,72 @@ off_t DicTreeNode::isMatchKeyword(char *str, off_t maxLen)
 class StringFilter
 {
 	public:
+		StringFilter();
+		~StringFilter();
+		void reset();
 		void addKeyword(std::string newKeyword);
 		void filter(std::string &strToBeReplace);
+		void buildKMP();
 
 	private:
-		std::vector< std::string > keywords;
-		class DicTreeNode root;
+		class DicTreeNode *root;
 };
 
-void  StringFilter::addKeyword(std::string newKeyword)
+StringFilter::StringFilter()
+{
+	DicTreeNode::search_count = 0;
+	root = new DicTreeNode();
+}
+
+StringFilter::~StringFilter()
+{
+	delete root;
+}
+
+void StringFilter::reset()
+{
+	DicTreeNode::search_count = 0;
+	delete root;
+	root = new DicTreeNode();
+}
+
+void StringFilter::buildKMP()
+{
+	std::vector< class DicTreeNode * > DFS_nodesTravesal;
+	//intial
+	for (class DicTreeNode *child : root->childNodes) {
+		child->toFirstMatchNode = NULL;
+		for (class DicTreeNode *node : child->childNodes) {
+			DFS_nodesTravesal.push_back(node);
+		}
+	}
+	while(DFS_nodesTravesal.size() != 0) {
+		class DicTreeNode *node = DFS_nodesTravesal.back();
+		DFS_nodesTravesal.pop_back();
+
+		std::unordered_map< char, class DicTreeNode *>::iterator it;
+		if ( (it = root->map.find(node->value)) != root->map.end() ) {
+			//std::cout << "ZZZZ: node (" << node->value << "): FOUND" << std::endl;
+			node->toFirstMatchNode = it->second;
+		} else {
+			//std::cout << "ZZZZ: node (" << node->value << "): NOT FOUND" << std::endl;
+			node->toFirstMatchNode = NULL;
+		}
+		for (class DicTreeNode *n : node->childNodes) {
+			DFS_nodesTravesal.push_back(n);
+		}
+	}
+}
+
+void StringFilter::addKeyword(std::string newKeyword)
 {
 	char *szBuf = new char[newKeyword.size() + 1];
 	off_t pos, len;
 	DicTreeNode *node, *child;
 
-	this->keywords.push_back(newKeyword);
-
 	snprintf(szBuf, newKeyword.size() + 1, "%s", newKeyword.c_str());
 	len = pos = newKeyword.length();
-	node = root.findLeaf(szBuf, pos);
+	node = root->findLeaf(szBuf, pos);
 
 	//std::cout << "pos = " << pos << std::endl;
 	//std::cout << "len = " << len << std::endl;
@@ -125,6 +185,7 @@ void  StringFilter::addKeyword(std::string newKeyword)
 		//std::cout << szBuf[pos] << std::endl;
 		child = new DicTreeNode(szBuf[pos], pos);
 		node->addChildNode(szBuf[pos], child);
+		child->parentNode = node;
 		node = child;
 		pos++;
 	}
@@ -136,43 +197,143 @@ void StringFilter::filter(std::string &strToBeReplace)
 {
 	char *target = new char[strToBeReplace.size() + 1];
 	char *ptr = target;
+	char *out = NULL;
 	off_t ind = 0;
 	off_t maxlen = strToBeReplace.size();
 	off_t remain = maxlen;
+	class DicTreeNode *cur = root;
 
 	snprintf(target, strToBeReplace.size() + 1, "%s", strToBeReplace.c_str());
 	//std::cout << target << std::endl;
 
 	while(ind < maxlen) {
+		class DicTreeNode *next = cur->isMatchKeyword(ptr, remain, out);
 		//std::cout << "ind: "  << ind << std::endl;
 		//std::cout << "remain: "  << remain << std::endl;
-		off_t len = root.isMatchKeyword(ptr, remain);
-		//std::cout << "len: "  << len << std::endl;
-		for (off_t i = ind; i < ind + len; i++) {
-			target[i] = '*';
+		//std::cout << "XXXX: target = " << target << std::endl;
+		//std::cout << "XXXX: str = " << ptr << std::endl;
+		//std::cout << "XXXX: out = " << out << std::endl;
+		//if (next == NULL) {
+		//	std::cout << "XXXX: NOT FOUND!!!" << std::endl;
+		//} else {
+		//	std::cout << "XXXX: next->value = " << next->value << std::endl;
+		//}
+		ind = out - target + 1;
+		ptr = out;
+		ptr++;
+		if (next != NULL && next->getEnd()) {
+			//std::cout << "XXXX: found" << std::endl;
+			for (off_t i = 0; i <= next->length; i++) {
+				*(out - i - 1) = '*';
+			}
+			cur = root; // reset
+		} else if (next == NULL) {
+			//std::cout << "XXXX: IGNORE all char and start with next char" << std::endl;
+			cur = root;
+		} else { // keep searching
+			//std::cout << "XXXX: keep search" << std::endl;
+			ind = ind + next->length - cur->length;
+			cur = next;
 		}
-		//std::cout << target << std::endl;
-		ind = ind + len + 1;
-		ptr = ptr + len + 1;
 		remain = maxlen - ind;
 	}
 	strToBeReplace = target;
 	delete [] target;
 }
 
+//#define UT
+#ifdef UT
+#define UT_CASE_1
+#define UT_CASE_2
+#endif
+
 int main()
 {
-	StringFilter obj;
-	std::string input = "ABCDEFGHIJKLMNOPQRSTUVWXYZ, ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	obj.addKeyword("ABC");
-	obj.addKeyword("ABCDEG");
-	obj.addKeyword("IJK");
-	obj.addKeyword("XYZ");
+#ifdef UT
+#ifdef UT_CASE_1
+	{
+		StringFilter obj;
+		std::string input = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		std::string result = "****EFGH***LMNOPQRSTUVW***";
+		obj.addKeyword("ABCD");
+		obj.addKeyword("IJK");
+		obj.addKeyword("XYZ");
+		obj.buildKMP();
 
-	std::cout << input << std::endl;
-	obj.filter(input);
-	std::cout << input << std::endl;
-	std::cout << "search count: " << DicTreeNode::search_count << std::endl;
+		std::cout << "UT case 1" << std::endl;
+		std::cout << input << std::endl;
+		obj.filter(input);
+		std::cout << input << std::endl;
+		std::cout << result << std::endl;
+		if (input == result) {
+			std::cout << "Success" << std::endl;
+		} else {
+			std::cout << "Failed" << std::endl;
+		}
+		std::cout << "input length: " << input.size() << "; search count: " << DicTreeNode::search_count << std::endl;
+	}
+#endif //UT_CASE_1
+#ifdef UT_CASE_2
+	{
+		StringFilter obj;
+		std::string input = "ABCDEF";
+		std::string result = "ABCDE*";
+		obj.addKeyword("ABCDEG");
+		obj.addKeyword("BCDEG");
+		obj.addKeyword("CDEG");
+		obj.addKeyword("DEG");
+		obj.addKeyword("EG");
+		obj.addKeyword("F");
+		obj.buildKMP();
+
+		std::cout << "UT case 2" << std::endl;
+		std::cout << input << std::endl;
+		obj.filter(input);
+		std::cout << input << std::endl;
+		std::cout << result << std::endl;
+		if (input == result) {
+			std::cout << "Success" << std::endl;
+		} else {
+			std::cout << "Failed" << std::endl;
+		}
+		std::cout << "input length: " << input.size() << "; search count: " << DicTreeNode::search_count << std::endl;
+	}
+#endif //UT_CASE_2
+#endif //UT
+
+	StringFilter obj;
+	std::string input;
+	std::ifstream keyword_file("./test/keywords");
+	std::ifstream filter_file("./test/11-0.txt");
+
+	std::ofstream result;
+	std::ofstream stat;
+
+	result.open("./test/result.txt");
+	stat.open("./test/stat.txt");
+
+	stat << "input_length	search_count"<< std::endl;
+
+	while (std::getline(keyword_file, input))
+	{
+		//std::cout << input << std::endl;
+		obj.addKeyword(input);
+	}
+
+	while (std::getline(filter_file, input))
+	{
+		if (input.size() == 0) {
+			result << input << std::endl;
+			continue;
+		}
+		DicTreeNode::search_count = 0;
+		std::cout << input << std::endl;
+		obj.filter(input);
+		std::cout << input << std::endl;
+		result << input << std::endl;
+		stat << input.size() << "	" << DicTreeNode::search_count << std::endl;
+	}
+
 
 	return 0;
 }
---
+++ test/parse.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import os
+import sys as sys
+import re
+
+reg=re.compile('^[a-zA-Z]+$')
+
+with open('pg29765.txt') as f:
+    for line in f:
+        if len(line.split()) == 1:
+            if reg.match(line) and line:
+                line = re.sub('\n', '', line)
+                print(line)
+
