bde4242b028dad0da620fa86d7f09d28a7b81e22
Sheng-Chien Lin <mlsaint1214@gmail.com>
2018-12-06 00:12:31 +0800

    modify keyword length > 5

--- test/parse.py
+++ test/parse.py
@@ -9,7 +9,7 @@ reg=re.compile('^[a-zA-Z]+$')
 with open('pg29765.txt') as f:
     for line in f:
         if len(line.split()) == 1:
-            if reg.match(line) and line:
+            if reg.match(line) and len(line) > 5:
                 line = re.sub('\n', '', line)
                 print(line)
 
